5cf55a01642ad61f65ad4ea459a2edabff82ebdc
kwagstyl <konrad.wagstyl@gmail.com>
2021-01-26 15:36:07 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='matplotlib_surface_plotting',
+      version='0.1',
+      packages=find_packages(),
+      install_requires=['nibabel',
+                        'matplotlib>=3.3.2'],
+      package_dir={'matplotlib_surface_plotting':'matplotlib_surface_plotting'},
+     )
+
